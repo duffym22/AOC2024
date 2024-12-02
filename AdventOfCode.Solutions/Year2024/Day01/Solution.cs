fa84e6578664b1a47eca43e93d039dcd0ca2d891
Matthew Duffy <duffym22@gmail.com>
2024-12-01 22:04:43 -0600

    Day 1 performance updates

--- AdventOfCode.Solutions/Year2024/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day01/Solution.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.IO.IsolatedStorage;
 
 namespace AdventOfCode.Solutions.Year2024.Day01;
@@ -9,19 +10,7 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
-        List<string> ParsedInput = Input.SplitByNewline().ToList();
-        List<int> Left = new List<int>();
-        List<int> Right = new List<int>();
-
-        foreach (string item in ParsedInput)
-        {
-            string[] sp = item.Split(" ");
-            Left.Add(int.Parse(sp.First()));
-            Right.Add(int.Parse(sp.Last()));
-        }
-
-        Left.Sort();
-        Right.Sort();
+        ParseAndSort(out List<int> Left, out List<int> Right);
 
         int totalDistance = 0;
         for (int i = 0; i < Left.Count; i++)
@@ -36,9 +25,39 @@ class Solution : SolutionBase
 
     protected override string SolvePartTwo()
     {
+        ParseAndSort(out List<int> Left, out List<int> Right);
+        int similarity = 0;
+        // Original - executes in 42.2 ms
+        //for (int i = 0; i < Left.Count; i++)
+        //{
+        //    similarity += Left[i] * Right.Count(x => x == Left[i]);
+        //}
+
+        // Executes in 1.2 ms
+        Dictionary<int, int> keyValuePairs = new();
+        foreach (int i in Right)
+        {
+            if (keyValuePairs.ContainsKey(i))
+                keyValuePairs[i] += 1;
+            else
+                keyValuePairs.Add(i, 1);
+        }
+
+        foreach (int item in Left)
+        {
+            if(keyValuePairs.ContainsKey(item))
+                similarity += item * keyValuePairs[item];
+        }
+
+        //Attempt 1: 22545250 - CORRECT
+        return similarity.ToString();
+    }
+
+    private void ParseAndSort(out List<int> Left, out List<int> Right)
+    {
         List<string> ParsedInput = Input.SplitByNewline().ToList();
-        List<int> Left = new List<int>();
-        List<int> Right = new List<int>();
+        Left = new List<int>();
+        Right = new List<int>();
 
         foreach (string item in ParsedInput)
         {
@@ -49,13 +68,5 @@ class Solution : SolutionBase
 
         Left.Sort();
         Right.Sort();
-        int similarity = 0;
-        for (int i = 0; i < Left.Count; i++)
-        {
-            similarity += Left[i] * Right.Count(x => x == Left[i]);
-        }
-
-        //Attempt 1: 22545250 - CORRECT
-        return similarity.ToString();
     }
 }
