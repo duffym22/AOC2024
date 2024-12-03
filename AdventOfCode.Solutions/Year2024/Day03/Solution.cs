30430505c28c20794bf7452e180dd04c70587124
Matthew Duffy <mduffy@ra.rockwell.com>
2024-12-03 12:28:28 -0600

    Day 3 solved

--- AdventOfCode.Solutions/Year2024/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day03/Solution.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Solutions.Year2024.Day03;
 
 class Solution : SolutionBase
@@ -6,11 +8,58 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
-        return "";
+        Regex mulEx = new Regex("mul\\([0-9]{1,4}\\,[0-9]{1,4}\\)");
+        MatchCollection matchCollection = mulEx.Matches(Input);
+        int sumMulInstructions = 0;
+        foreach (Match item in matchCollection)
+        {
+            sumMulInstructions += GetProduct(item.ToString());
+        }
+
+        //Attempt 1: 173731097 - CORRECT
+        return sumMulInstructions.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        Regex mulEx = new Regex("(do|don\'t|mul)\\(([0-9]{1,4}\\,[0-9]{1,4})?\\)");
+        MatchCollection matchCollection = mulEx.Matches(Input);
+        int sumMulInstructions = 0;
+        bool mulEnabled = true;
+
+        foreach (Match item in matchCollection)
+        {
+            string conditional = item.ToString();
+            if (conditional.StartsWith("don"))
+            {
+                mulEnabled = false;
+                continue;
+            }
+            
+            if (conditional.StartsWith("do("))
+            {
+                mulEnabled = true;
+                continue;
+            }
+
+            if (mulEnabled)
+            {
+                sumMulInstructions += GetProduct(item.ToString());
+            }
+        }
+
+        //Attempt 1: 93729253 - CORRECT
+        return sumMulInstructions.ToString();
+    }
+
+    private int GetProduct(string mulValues)
+    {
+        Regex valuesOnly = new Regex("[0-9]{1,4}\\,[0-9]{1,4}");
+        MatchCollection theDeets = valuesOnly.Matches(mulValues);
+        string[] workingString = theDeets.First().ToString().Split(',');
+        int.TryParse(workingString.First(), out int val1);
+        int.TryParse(workingString.Last(), out int val2);
+        return val1 * val2;
+
     }
 }
